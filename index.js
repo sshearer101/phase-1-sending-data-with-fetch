d7c6295eb774875cdc5ecc70012d25ccfd8aaf11
Sam Shearer <sshearer101@gmail.com>
2021-07-30 18:16:39 -0600

    commit tree e024c2400717a0caa2eb5e910f4971b387b032a3

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+
+function submitData(name,email){
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+
+        },
+        body: JSON.stringify(name, email)
+    })
+
+    .then(json = json())
+    .then(object = document.body.innerHTML = object["id"])
+    .catch (error = document.body.innerHTML = error)
+
+    }
+
+
+
+// function submitData(name, email){
+//     return fetch('http://localhost:3000/users',  {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({name, email})
+
+//      })
+
+//      .then(res = res.json())
+//      .then(object => document.body.innerHTML = object[ "id" ])
+//      .catch (error => document.body.innerHTML = error)
+// }
